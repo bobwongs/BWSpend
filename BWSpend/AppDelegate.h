9ca35ec6c3620ea7bedd32651bf9a35a4345215a
wangbo <wangbo@bluemoon.com.cn>
2016-08-17 11:22:41 +0800

    Upload Project

---
+++ BWSpend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWSpend
+//
+//  Created by BobWong on 16/8/17.
+//  Copyright © 2016年 BobWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BWSpend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BWSpend
+//
+//  Created by BobWong on 16/8/17.
+//  Copyright © 2016年 BobWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
